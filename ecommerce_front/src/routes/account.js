d5ad1eeb07326510074edc280d799ab29c55642b
matteus.vargas <vargas.simao@gmail.com>
2020-05-05 18:01:49 -0300

    Mudanças

---
+++ ecommerce_front/src/routes/account.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    return res.render('account/index', {
+        title: 'Account',
+        layout: 'layouts/main'
+    })
+})
+
+router.get('/new', (req, res) => {
+    return res.render('account/new', {
+        title: 'Account Register',
+        layout: 'layouts/main'
+    })
+})
+
+module.exports = router
--- ecommerce_front/src/routes/main.js
+++ ecommerce_front/src/routes/main.js
@@ -2,10 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-	return res.render('main/index', {
-		title: 'Test Ecommerce',
-		layout: 'layouts/base'
-	})
+    return res.render('main/index', {
+        title: 'Test Ecommerce',
+        layout: 'layouts/base'
+    })
 })
 
 module.exports = router
